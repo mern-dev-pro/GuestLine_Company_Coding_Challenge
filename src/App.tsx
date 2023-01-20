f6131482d93d6b1b01d58277318dff794920b5a0
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2023-01-20 15:25:45 +0200

    Basic Structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Text from 'components/Text';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is just testing</p>
+        <Text />
+      </header>
+    </div>
+  );
+}
+
+export default App;
