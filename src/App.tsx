352e182b0ae78d62424d1b1bd5fac0dbb123d24c
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2023-01-20 16:46:19 +0200

    CREATE: Rating Stars Componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
+
 import Text from 'components/Text';
-import React from 'react';
-import './App.css';
+import Container from 'layout/Container';
+import Rating from 'components/Rating';
 
 function App() {
+  const [stars, setStars] = useState(4);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>This is just testing</p>
-        <Text />
+    <div>
+      <header>
+        <Container>
+          <Text size="title" content="This is just testing for text component" />
+          <Text size="heading" content="This is just testing for text component" />
+          <Text size="pharagraph" content="This is just testing for text component" />
+          <Rating rating={stars} changeRating={setStars} />
+        </Container>
       </header>
     </div>
   );
---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import StarRatings from 'react-star-ratings';
+
+type RatingProps = {
+  rating: number;
+  changeRating: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Rating: React.FC<RatingProps> = ({ rating, changeRating }) => {
+  return (
+    <div>
+      <StarRatings
+        rating={rating}
+        changeRating={changeRating}
+        starRatedColor="yellow"
+        starHoverColor="yellow"
+        numberOfStars={5}
+        name="rating"
+      />
+    </div>
+  );
+};
+
+export default Rating;
