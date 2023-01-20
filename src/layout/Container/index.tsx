5f49b5e1fed60f2b2cd8855739fa69d8a52e3aca
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2023-01-20 17:11:14 +0200

    UPDATE: Container Componet For Additional Class

--- src/layout/Container/index.tsx
+++ src/layout/Container/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import styles from './styles.module.scss';
 
 type ContainerProps = {
   children: React.ReactNode;
+  additionalClass?: string;
 };
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
+const Container: React.FC<ContainerProps> = ({ children, additionalClass }) => {
+  return <div className={classnames(styles.wrapper, additionalClass)}>{children}</div>;
 };
 
 export default Container;
