8cfd7607a2045847c41f1ae05df2a7f643a5cfb6
Mykhailo Romaniuk <mykhailo@nerdzz.digital>
2023-01-23 02:31:48 +0200

    UPD: Link real data to component, configure responsive

---
+++ src/components/UI/ListItem/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+import Text from 'components/Basic/Text';
+import Rating from 'components/Basic/Rating';
+import { HotelType } from 'pages/Home';
+
+import styles from './styles.module.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+type ListItemProps = {
+  hotel: HotelType;
+};
+
+const ListItem: React.FC<ListItemProps> = ({ hotel }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <div className={styles.carousel}>
+          <Carousel showThumbs={false} showIndicators={false} autoPlay infiniteLoop>
+            {hotel?.images?.map((image, index) => (
+              <div key={index}>
+                <img src={image.url} />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+        <div className={styles.headerContent}>
+          <div className={styles.name}>
+            <Text content={hotel?.name} size="heading" additionalClass={styles.hotelName} />
+            <Text content={hotel?.address1} size="pharagraph" />
+            <Text content={hotel?.address2} size="pharagraph" />
+            <Text content={hotel?.town} size="pharagraph" />
+            <Text content={hotel?.country} size="pharagraph" />
+          </div>
+          <div className={styles.rating}>
+            <Rating rating={parseInt(hotel?.starRating)} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.body}>
+        {hotel?.rooms?.map(room => (
+          <div className={styles.room} key={room?.id}>
+            <div className={styles.name}>
+              <Text content={room?.name} size="pharagraph" additionalClass={styles.roomName} />
+              <Text content={`Adults: ${room?.occupancy?.maxAdults}`} size="pharagraph" />
+              <Text content={`Children: ${room?.occupancy?.maxChildren}`} size="pharagraph" />
+            </div>
+            <div className={styles.bodyContent}>
+              <Text content={room?.longDescription} size="pharagraph" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ListItem;
